093f2aa6f120cd317dbaaebf6f89cde8aa7cf2a9
Eddisonandres <97772229+Eddisonandres@users.noreply.github.com>
2024-04-03 00:14:35 +0000

    start2

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import os
 api_key = os.getenv("NREL_API_KEY")
 # Perform a GET request using Requests
 
-req_url = "developer.nrel.gov/"
-endpoint_url = "api/alt-fuel-stations/v1.json?{}".format(api_key)
-req_url = req_url + endpoint_url
+req_url = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?{}".format(api_key)
+#endpoint_url = "api/alt-fuel-stations/v1.json?{}".format(api_key)
+#req_url = req_url + endpoint_url
 
 response = requests.get(req_url)
 
